ed8c612f6e74b97ee0fdba48a85109c2b909317f
limseunghyun95 <limseunghyun95@gmail.com>
2024-08-31 03:37:41 +0900

    Add hackerrank problem_solving drawing_book

---
+++ hackerrank/problem_solving/drawing_book.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+
+def pageCount(n, p):
+    total_search_count = n // 2
+    front_search_count = p // 2
+
+    # right_search_count = total_search_count - front_search_count
+    return min(front_search_count, total_search_count - front_search_count)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
